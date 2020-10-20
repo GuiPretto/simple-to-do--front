2b093ae96d40f0cec97655230e66fb1eec332a4e
Guilherme Cândido <guipretto5@gmail.com>
2020-10-20 12:43:47 -0300

    Creates and stylizes Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Wrapper, Title } from './style'
+
+const Header = () => (
+  <Wrapper>
+    <Title>Simple To-Do App</Title>
+  </Wrapper>
+)
+
+export default Header
