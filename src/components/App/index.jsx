5acf313a8b4175eefb9fd28987cd7d6a98238df9
Guilherme Cândido <guipretto5@gmail.com>
2020-10-20 15:15:05 -0300

    Adds DnD Provider on App

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import Header from '../Header';
 import Main from '../Main';
 import store from '../../store'
 
 const App = () => (
   <Provider store={store}>
-    <Header/>
-    <Main/>
+    <DndProvider backend={HTML5Backend}>
+      <Header/>
+      <Main/>
+    </DndProvider>
   </Provider>
 )
 
